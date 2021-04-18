a90389829895f62afcb3d12badc378a3890acd9d
Konrad <konrad.danko@gmail.com>
2021-04-18 18:22:15 +0200

    kyu8 StringUtilsIisDigit

---
+++ src/practices/codewars/kyu8/StringUtilsIisDigit.java
@@ -0,0 +1,9 @@
+package practices.codewars.kyu8;
+
+public class StringUtilsIisDigit {
+
+  public static boolean isDigit(String s) {
+    // your code goes here
+    return s.matches("^\\d$");
+  }
+}
