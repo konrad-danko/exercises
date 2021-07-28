406feef98480a9266bf256b2e879f6ccd8afa45a
Konrad <konrad.danko@gmail.com>
2021-07-28 08:15:53 +0200

    kyu4 DifferentiateAPolynomial

---
+++ src/practices/codewars/kyu4/DifferentiateAPolynomial.java
@@ -0,0 +1,38 @@
+package practices.codewars.kyu4;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class DifferentiateAPolynomial {
+  public static BigInteger differentiate(String equation, long x) {
+    // Your code here!
+    return Arrays.stream(equation
+        .replaceAll("-", "|-")
+        .replaceAll("\\+", "|+")
+        .split("\\|"))
+        .filter(str -> str.contains("x"))
+        .map(str-> calcDiff(str, x))
+        .reduce(new BigInteger("0"), BigInteger::add);
+  }
+
+  private static BigInteger calcDiff(String str, long x){
+    String strUpToX = str.substring(0, str.indexOf("x"));
+    String coefficientStr = strUpToX.equals("-") || strUpToX.equals("+") || strUpToX.equals("")?
+        strUpToX + "1":
+        strUpToX;
+    BigInteger coefficient = new BigInteger(coefficientStr);
+    String powerStr = str.contains("^") ? str.substring(str.indexOf("^")+1) : "1";
+    BigInteger power = new BigInteger(powerStr);
+    return power.multiply(coefficient).multiply(BigInteger.valueOf(x).pow(power.intValue()-1));
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println(differentiate("12x+2", 3));
+    System.out.println(differentiate("x^2-x", 3));
+    System.out.println(differentiate("-5x^2+10x+4", 3));
+    System.out.println(differentiate("-21x^5+3x^3",12398));
+    System.out.println(differentiate("x^2",59884848483559L));
+    System.out.println(differentiate("-7x^5+22x^4-55x^3-94x^2+87x-56",-3));
+  }
+}
