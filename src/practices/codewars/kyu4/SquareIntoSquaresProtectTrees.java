21f66dabd96ea13bb527c58145bb618184a2777f
Konrad <konrad.danko@gmail.com>
2022-03-15 21:10:53 +0100

    kyu4 SquareIntoSquaresProtectTrees

---
+++ src/practices/codewars/kyu4/SquareIntoSquaresProtectTrees.java
@@ -0,0 +1,14 @@
+package practices.codewars.kyu4;
+
+public class SquareIntoSquaresProtectTrees {
+  public String decompose(long n) {
+
+    return "";
+  }
+
+  public static void main(String[] args) {
+    String qqq = "11111";
+    System.out.println(Integer.parseInt(qqq, 2));
+    System.out.println(Integer.toBinaryString(31));
+  }
+}
