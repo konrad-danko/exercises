5afa582d2f86ccc5234549b074c939e3bece80a5
Konrad <konrad.danko@gmail.com>
2022-03-15 23:19:32 +0100

    Commit of TestClass01

--- src/practices/git_testing/TestClass01.java
+++ src/practices/git_testing/TestClass01.java
@@ -1,2 +1,7 @@
-package practices.git_testing;public class TestClass01 {
+package practices.git_testing;
+
+public class TestClass01 {
+  public static void main(String[] args) {
+    System.out.println("Hi TestClass01");
+  }
 }
