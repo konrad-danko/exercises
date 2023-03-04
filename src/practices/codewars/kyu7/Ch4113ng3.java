2250189a49fea2c66a422b7b1bda58cb68e1aa4a
konrad-danko <konrad.danko@gmail.com>
2023-03-04 15:51:43 +0100

    kyu7 Ch4113ng3

---
+++ src/practices/codewars/kyu7/Ch4113ng3.java
@@ -0,0 +1,10 @@
+package practices.codewars.kyu7;
+
+public class Ch4113ng3 {
+  public static String nerdify(String txt){
+    return txt
+        .replaceAll("[aA]", "4")
+        .replaceAll("[eE]", "3")
+        .replaceAll("[l]", "1");
+  }
+}
